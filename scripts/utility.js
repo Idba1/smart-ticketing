d9198927ba0b0993b291bf54e75804eb3a0057a2
Idba1 <moniraislamself@gmail.com>
2024-02-19 14:19:59 +0600

    seat 4

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,4 +1,4 @@
-function setInnerText(id, value){
+function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
 
@@ -6,26 +6,30 @@ function setInnerText(id, value){
 const allSeat = document.getElementsByClassName('seat-btn');
 let count = 0;
 
-for (const seat of allSeat){
-    seat.addEventListener('click', function(e){
-        count = count+1;
-        document.getElementById('selected-seat').innerText = count;
+for (const seat of allSeat) {
+    seat.addEventListener('click', function (e) {
+        if (count < 4) {
+            count = count + 1;
+            document.getElementById('selected-seat').innerText = count;
+        }
     })
 }
 
 
 
 
-function resetSeatText(id, value){
+function resetSeatText(id, value) {
     document.getElementById(id).innerText = value;
 }
 
 const resetSeat = document.getElementsByClassName('seat-btn');
 let available = 8;
 
-for (const seat of resetSeat){
-    seat.addEventListener('click', function(e){
-        available = available-1;
-        document.getElementById('available').innerText = available;
+for (const seat of resetSeat) {
+    seat.addEventListener('click', function (e) {
+        if (available <= 8 && available >= 5) {
+            available = available - 1;
+            document.getElementById('available').innerText = available;
+        }
     })
 }
